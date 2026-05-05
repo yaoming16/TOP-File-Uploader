7da07eb99339489450d3446da23d70096a67134d
Pablo Perez <pabloperezzz16@gmail.com>
2026-05-05 18:43:35 +0000

    Improved file page style. Noe each column will have a minimum width, so the layout will be more consistent across different screen sizes. Added a "More Info" that open by clicking a button in the options menu. The modal shows the full information of the file/folder.

--- public/scripts/filesUI.js
+++ public/scripts/filesUI.js
@@ -7,10 +7,10 @@ const folderContentContainer = document.getElementById(
 //Function to open the options menu
 function openOptionsMenu(e) {
   //if (e.target.classList.contains('file-container')) return;
-  
+
   // Whe get the nearest file-container from where the user clicked (usually should be the parent article of the element the user clicked)
   const clickedItem = e.target.closest(".file-container");
-  
+
   if (clickedItem) {
     // Now we save the id of the file/folder and type on the menu data-id and data-type attribute for future use
     e.preventDefault();
@@ -18,13 +18,17 @@ function openOptionsMenu(e) {
     const type = clickedItem.dataset.type;
     const name = clickedItem.dataset.name;
     const extension = clickedItem.dataset.extension;
-  
+    const date = clickedItem.dataset.date;
+    const size = clickedItem.dataset.size;
+
     menu.setAttribute("data-id", id);
     menu.setAttribute("data-type", type);
     menu.setAttribute("data-name", name);
-  
+    menu.setAttribute("data-date", date);
+
     if (extension) menu.setAttribute("data-extension", extension);
-  
+    if (size) menu.setAttribute("data-size", size);
+
     // Show the menu
     menu.style.display = "block";
     menu.style.left = `${e.pageX}px`;
@@ -36,8 +40,11 @@ folderContentContainer.addEventListener("contextmenu", openOptionsMenu);
 
 //If user clicks anywhere else we hide the menu
 document.addEventListener("click", (e) => {
-  //Close only if already visible and clicked DOM element isnt the button to open the menu 
-  if (menu.style.display === "block" && !e.target.classList.contains("optionsBtn")) {
+  //Close only if already visible and clicked DOM element isnt the button to open the menu
+  if (
+    menu.style.display === "block" &&
+    !e.target.classList.contains("optionsBtn")
+  ) {
     menu.style.display = "none";
   }
 });
@@ -64,9 +71,27 @@ openUpdateModalBtn.addEventListener("click", () => {
 
 //Open options menu using the on screen Btn
 folderContentContainer.addEventListener("click", (e) => {
-
   if (e.target.classList.contains("optionsBtn")) {
     openOptionsMenu(e);
   }
 });
 
+// More info modal
+const openInfoModalBtn = document.getElementById("openInfoModalBtn");
+
+openInfoModalBtn.addEventListener("click", () => {
+  const fileName = menu.dataset.name;
+  const extension = menu.dataset.extension;
+  const date = menu.dataset.date;
+  const size = menu.dataset.size;
+
+  document.getElementById("infoFileName").textContent = extension
+    ? fileName + extension
+    : fileName;
+  document.getElementById("infoFileDate").textContent = "Created at: " + date;
+  if (size)
+    document.getElementById("infoFileSize").textContent = "Size: " + size;
+  if (extension) {
+    document.getElementById("infoFileType").textContent = "Type: " + extension;
+  }
+});
